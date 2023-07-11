46ec0782b4c7cc21cd37f1232e11c05372a364a8
Alexander <alex.ngbr@googlemail.com>
2023-07-11 11:38:36 +0200

    Updates to Navigation Script

---
+++ Assets/Scripts/NavigationTask/Navigation_MainScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation_MainScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //For measuring user's orientation, use relative position of headset to controllers (as well as headset orientation and controller orientation) - this allows to turn the head without the software falsely registering a change in body orientation.
+    }
+}
