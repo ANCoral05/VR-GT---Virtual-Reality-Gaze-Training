e41cbfe9aceb2d60f96b013dd5c45f216860bdbc
Alexander <a.neugebauer@uni-tuebingen.de>
2025-01-15 13:33:14 +0100

    Core Framework - Scriptable Object based Variable Architecture 005
    Advanced SO variable functionality on the example of a boolean

---
+++ Assets/Scripts/CoreScripts/DamageableEntityScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRK_BuildingBlocks;
+
+public class DamageableEntityScript : MonoBehaviour
+{
+    [SerializeField]
+    private FloatVariable health;
+}
