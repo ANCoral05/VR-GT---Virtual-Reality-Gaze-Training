8c5770643dec050af26ae500e4fe73043a376bea
Alexander <a.neugebauer@uni-tuebingen.de>
2025-01-29 16:16:01 +0100

    Core Framework - Save and Load Functionality 004
    Implemented different save conditions.

--- Assets/Scripts/CoreScripts/SaveAndLoadManager.cs
+++ Assets/Scripts/CoreScripts/SaveAndLoadManager.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 using System.Reflection;
 using System.Text;
 using System;
+using System.IO;
+
+
+/// Summary:
+/// Manages the saving and loading of data from ScriptableObjects to a JSON file.
+
+/// Remarks:
+/// The ScriptableObjects to be saved are added to the storedParameterList.
+/// Only public or serialized private fields or properties of the ScriptableObjects are saved.
+/// ScriptableObjects are identified by their name, and the data is saved in the order of the ScriptableObjects in the list.
+/// Loading a file that uses a different order of ScriptableObjects than the current project can result in failure.
 
 public class SaveAndLoadManager : MonoBehaviour
 {
+    // an enum to determine when to save or load, with options for "On variable change", "At fixed intervals", and "Manually"
+    public enum SaveTrigger
+    {
+        OnVariableChange,
+        AtFixedTimeIntervals,
+        AtFixedFrameIntervals,
+        Manually
+    }
+
+    [Header("Save and Load Settings")]
+    [SerializeField, Tooltip("When to save or load data")]
+    private SaveTrigger saveTrigger = SaveTrigger.Manually;
+
     [SerializeField, Tooltip("List of ScriptableObjects to be saved")]
     private List<ScriptableObject> storedParameterList = new List<ScriptableObject>();
 
@@ -16,54 +40,71 @@ public class SaveAndLoadManager : MonoBehaviour
     [SerializeField, Tooltip("Load File Name (If not set, default Load File Name is used)")]
     private string loadFileName;
 
-    private void Update()
-    {
-        //if (Input.GetKeyDown(KeyCode.S))
-        //{
-        //    CreateNewSave();
-        //}
+    [SerializeField, Tooltip("Fixed time interval to store the save file.")]
+    private float fixedTimeInterval = 10f;
 
-        //if (Input.GetKeyDown(KeyCode.L))
-        //{
-        //    LoadSave();
-        //}
+    [SerializeField, Tooltip("Fixed frame interval to store the save file.")]
+    private int fixedFrameInterval = 1;
+
+    [SerializeField, Tooltip("If checked, a save file will created on initialization. Otherwise, the first save file will be created as soon as the save trigger is met.")]
+    private bool saveOnInitialization;
+
+    private float fixedTimeStamp;
+
+    private int fixedFrameStamp;
+
+    private const string DefaultFileName = "saveData.json";
+
+    public void Initialize()
+    {
+        if (saveTrigger == SaveTrigger.OnVariableChange)
+        {
+            SubscribeToOnChanged();
+        }
+
+        if (saveOnInitialization)
+        {
+            CreateNewSave();
+        }
     }
 
-    // A function that takes data from scriptable objects and saves it to a json file
     public void CreateNewSave()
     {
-        StringBuilder sb = new StringBuilder();
+        var sb = new StringBuilder();
         foreach (var scriptableObject in storedParameterList)
         {
-            sb.AppendLine($"ScriptableObject: {scriptableObject.name}");
-            var fields = scriptableObject.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            var properties = scriptableObject.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-            foreach (var field in fields)
-            {
-                var value = field.GetValue(scriptableObject);
-                sb.AppendLine($"{field.Name}: {value}");
-            }
-
-            foreach (var property in properties)
-            {
-                if (property.CanRead)
-                {
-                    var value = property.GetValue(scriptableObject);
-                    sb.AppendLine($"{property.Name}: {value}");
-                }
-            }
+            AppendScriptableObjectData(sb, scriptableObject);
         }
 
         string saveData = sb.ToString();
         Debug.Log("Data saved!");
 
-        // Save to file
-        string saveFileName_ext = (saveFileName ?? "saveData") + ".json";
-        System.IO.File.WriteAllText(saveFileName_ext, saveData);
+        string saveFileNameExt = string.IsNullOrEmpty(saveFileName) ? DefaultFileName : $"{saveFileName}.json";
+        File.WriteAllText(saveFileNameExt, saveData);
     }
 
-    // A function that sets the value of a field or property of a scriptable object
+    private void AppendScriptableObjectData(StringBuilder sb, ScriptableObject scriptableObject)
+    {
+        sb.AppendLine($"ScriptableObject: {scriptableObject.name}");
+        var fields = scriptableObject.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        var properties = scriptableObject.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (var field in fields)
+        {
+            var value = field.GetValue(scriptableObject);
+            sb.AppendLine($"{field.Name}: {value}");
+        }
+
+        foreach (var property in properties)
+        {
+            if (property.CanRead)
+            {
+                var value = property.GetValue(scriptableObject);
+                sb.AppendLine($"{property.Name}: {value}");
+            }
+        }
+    }
+
     private void SetFieldOrPropertyValue(object obj, string name, string value)
     {
         var type = obj.GetType();
@@ -71,38 +112,53 @@ public class SaveAndLoadManager : MonoBehaviour
         var field = type.GetField(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
         if (field != null)
         {
-            var targetType = field.FieldType;
-            var convertedValue = targetType.IsEnum
-                ? Enum.Parse(targetType, value)
-                : Convert.ChangeType(value, targetType);
-            field.SetValue(obj, convertedValue);
+            SetFieldValue(field, obj, value);
             return;
         }
 
         var property = type.GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
         if (property != null && property.CanWrite)
         {
-            var targetType = property.PropertyType;
-            var convertedValue = targetType.IsEnum
-                ? Enum.Parse(targetType, value)
-                : Convert.ChangeType(value, targetType);
-            property.SetValue(obj, convertedValue);
+            SetPropertyValue(property, obj, value);
         }
     }
 
-    // A function that reads data from a json file and assigns it to scriptable objects
+    private void SetFieldValue(FieldInfo field, object obj, string value)
+    {
+        var targetType = field.FieldType;
+        var convertedValue = ConvertValue(targetType, value);
+        field.SetValue(obj, convertedValue);
+    }
+
+    private void SetPropertyValue(PropertyInfo property, object obj, string value)
+    {
+        var targetType = property.PropertyType;
+        var convertedValue = ConvertValue(targetType, value);
+        property.SetValue(obj, convertedValue);
+    }
+
+    private object ConvertValue(Type targetType, string value)
+    {
+        return targetType.IsEnum ? Enum.Parse(targetType, value) : Convert.ChangeType(value, targetType);
+    }
+
     public void LoadSave()
     {
-        string loadFileName_ext = (loadFileName ?? "saveData") + ".json";
+        string loadFileNameExt = string.IsNullOrEmpty(loadFileName) ? DefaultFileName : $"{loadFileName}.json";
 
-        if (!System.IO.File.Exists(loadFileName_ext))
+        if (!File.Exists(loadFileNameExt))
         {
             Debug.LogWarning("Save file not found!");
             return;
         }
 
-        string loadData = System.IO.File.ReadAllText(loadFileName_ext);
+        string loadData = File.ReadAllText(loadFileNameExt);
+        ParseAndLoadData(loadData);
+        Debug.Log("Data loaded!");
+    }
 
+    private void ParseAndLoadData(string loadData)
+    {
         string[] lines = loadData.Split('\n');
         ScriptableObject currentScriptableObject = null;
 
@@ -126,7 +182,46 @@ public class SaveAndLoadManager : MonoBehaviour
                 SetFieldOrPropertyValue(currentScriptableObject, fieldName, fieldValue);
             }
         }
+    }
 
-        Debug.Log("Data loaded!");
+    private void SubscribeToOnChanged()
+    {
+        foreach (ScriptableObject parameter in storedParameterList)
+        {
+            // TODO: Implement a way to subscribe to the OnChanged event of each variable component
+
+            // subscribe to the OnChanged event of each variable component
+            //if (parameter is IVariableComponent variableComponent)
+            //{
+            //    variableComponent.OnChanged += CreateNewSave;
+            //}
+
+            // subscribe to the OnChanged event of each variable component by finding the event by string
+            var eventInfo = parameter.GetType().GetEvent("OnChanged");
+            if (eventInfo != null)
+            {
+                var methodInfo = GetType().GetMethod("CreateNewSave", BindingFlags.NonPublic | BindingFlags.Instance);
+                var action = Delegate.CreateDelegate(eventInfo.EventHandlerType, this, methodInfo);
+                eventInfo.AddEventHandler(parameter, action);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (saveTrigger == SaveTrigger.AtFixedTimeIntervals && Time.fixedTime >= fixedTimeStamp )
+        {
+            CreateNewSave();
+            fixedTimeStamp = Time.fixedTime + fixedTimeInterval;
+        }
+    }
+
+    private void Update()
+    {
+        if (saveTrigger == SaveTrigger.AtFixedFrameIntervals && Time.frameCount >= fixedFrameStamp)
+        {
+            CreateNewSave();
+            fixedFrameStamp = Time.frameCount + fixedFrameInterval;
+        }
     }
 }
